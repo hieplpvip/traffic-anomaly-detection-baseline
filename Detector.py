963fa3e99a6ae9028d77319432190f30c5c86e80
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-07-31 16:39:59 -0400

    Reformat source code and markdown files

--- Detector.py
+++ Detector.py
@@ -17,14 +17,12 @@ from vid_utils import *
 with open('result.json', 'r') as f:
     D = json.load(f)
 
-
 #  ## Extract Objects
 
 # In[3]:
 
 
 All_Cords = extract_objects(D)
-
 
 # ## Extract Cases
 
@@ -33,7 +31,6 @@ All_Cords = extract_objects(D)
 
 AT = extract_cases(All_Cords)
 
-
 # ## Detect Change in Camera
 
 # In[6]:
@@ -41,17 +38,15 @@ AT = extract_cases(All_Cords)
 
 Base = "processed_images/"
 if path.exists("change.npy"):
-    change_cam,loc,Cstat = np.load("change.npy",allow_pickle=True)
+    change_cam, loc, Cstat = np.load("change.npy", allow_pickle=True)
 else:
-    change_cam, loc,Cstat = change_detect(Base)
-    np.save("change.npy",[change_cam,loc,Cstat])
-
+    change_cam, loc, Cstat = change_detect(Base)
+    np.save("change.npy", [change_cam, loc, Cstat])
 
 # In[7]:
 
 
 PT = list(set(AT) - set(change_cam))
-
 
 # ## Case 1: Extract ROI
 
@@ -59,37 +54,31 @@ PT = list(set(AT) - set(change_cam))
 
 
 if path.exists("centers1.npy"):
-    Centers = np.load("centers1.npy",allow_pickle=True)
+    Centers = np.load("centers1.npy", allow_pickle=True)
 else:
-    Centers = extract_roi(PT,All_Cords)
-    np.save("centers1.npy",Centers)
-
+    Centers = extract_roi(PT, All_Cords)
+    np.save("centers1.npy", Centers)
 
 # In[9]:
 
 
 len(Centers)
 
-
 # ## Case 1: Extract Bounds
 
 # In[10]:
 
 
-
-
 if path.exists("bounds1.npy"):
-    Bounds = np.load("bounds1.npy",allow_pickle=True)
+    Bounds = np.load("bounds1.npy", allow_pickle=True)
 else:
-    Bounds = extract_bounds(Centers,PT,All_Cords)
-    np.save("bounds1.npy",Bounds)
-
+    Bounds = extract_bounds(Centers, PT, All_Cords)
+    np.save("bounds1.npy", Bounds)
 
 # In[11]:
 
 
 len(Bounds)
-
 
 # ## Case 1: Backtracking
 
@@ -99,23 +88,21 @@ len(Bounds)
 Base = "ori_images/"
 
 if path.exists("result1.npy"):
-    Times, Stat = np.load("result1.npy",allow_pickle=True)
+    Times, Stat = np.load("result1.npy", allow_pickle=True)
 else:
-    Times, Stat = backtrack(Bounds,PT,Base)
-    np.save("result1.npy",[Times,Stat])
+    Times, Stat = backtrack(Bounds, PT, Base)
+    np.save("result1.npy", [Times, Stat])
 
-
-# ## Case 2: Extract ROI 
+# ## Case 2: Extract ROI
 
 # In[13]:
 
 
 if path.exists("centers2.npy"):
-    Centers2 = np.load("centers2.npy",allow_pickle=True)
+    Centers2 = np.load("centers2.npy", allow_pickle=True)
 else:
-    Centers2 = extract_roi1(change_cam,All_Cords,loc)
-    np.save("centers2.npy",Centers2)
-
+    Centers2 = extract_roi1(change_cam, All_Cords, loc)
+    np.save("centers2.npy", Centers2)
 
 # ## Case 2: Extract Bounds
 
@@ -123,66 +110,55 @@ else:
 
 
 if path.exists("bounds2.npy"):
-    Bounds2 = np.load("bounds2.npy",allow_pickle=True)
+    Bounds2 = np.load("bounds2.npy", allow_pickle=True)
 else:
-    Bounds2 = extract_bounds1(Centers2,change_cam,loc,All_Cords)
-    np.save("bounds2.npy",Bounds2)
-
+    Bounds2 = extract_bounds1(Centers2, change_cam, loc, All_Cords)
+    np.save("bounds2.npy", Bounds2)
 
 # ## Case 2: Backtracking
 
 # In[15]:
 
 
-len(Centers),len(Centers2)
-
+len(Centers), len(Centers2)
 
 # In[16]:
 
 
-len(Bounds),len(Bounds2)
-
+len(Bounds), len(Bounds2)
 
 # In[17]:
 
 
 Base = "ori_images/"
 
-
 if path.exists("result2.npy"):
-    Times2, Stat2 = np.load("result2.npy",allow_pickle=True)
+    Times2, Stat2 = np.load("result2.npy", allow_pickle=True)
 else:
-    Times2, Stat2 = backtrack1(Bounds2,Base)
-    np.save("result2.npy",[Times2,Stat2])
-
+    Times2, Stat2 = backtrack1(Bounds2, Base)
+    np.save("result2.npy", [Times2, Stat2])
 
 # In[18]:
 
 
-Times = {key:val for key, val in Times.items() if val != 999}
-Times = {key:val for key, val in Times.items() if val >= 40}
+Times = {key: val for key, val in Times.items() if val != 999}
+Times = {key: val for key, val in Times.items() if val >= 40}
 
-Times2 = {key:val for key, val in Times2.items() if val != 999}
-Times2 = {key:val for key, val in Times2.items() if val >= 40}
-
+Times2 = {key: val for key, val in Times2.items() if val != 999}
+Times2 = {key: val for key, val in Times2.items() if val >= 40}
 
 # In[23]:
 
 
-file1 = open("Result" + ".txt","w")
+file1 = open("Result" + ".txt", "w")
 for x in Times:
-    file1.write('{0:2d} {1:3d} {2:1d}'.format(x,int(Times[x]),1))
+    file1.write('{0:2d} {1:3d} {2:1d}'.format(x, int(Times[x]), 1))
     file1.write("\n")
-    
+
 for x in Times2:
-    file1.write('{0:2d} {1:3d} {2:1d}'.format(x,int(Times2[x]),1))
+    file1.write('{0:2d} {1:3d} {2:1d}'.format(x, int(Times2[x]), 1))
     file1.write("\n")
-    
+
 file1.close()
 
-
 # In[ ]:
-
-
-
-
