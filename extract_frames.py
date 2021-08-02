e83ee58f5989c9f5624fdb5ea24f1e260dcbe614
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-08-02 12:20:15 -0400

    Fix "TypeError: unsupported operand type(s) for -: 'NoneType' and 'int'"

--- extract_frames.py
+++ extract_frames.py
@@ -55,10 +55,11 @@ for video_path in tqdm.tqdm(videos_folder):
         img = cv2.imread(os.fspath(video_path / (str(start_frame) + '.jpg')))
         for n in range(nums_frames):
             now_im = cv2.imread(os.fspath(video_path / (str(n * internal_frame + start_frame) + '.jpg')))
-            filename = str(n * internal_frame + start_frame) + '_' + str(j) + '.jpg'
-            if np.mean(np.abs(now_im - former_im)) > 5:
-                img = img * (1 - alpha) + now_im * alpha
-                cv2.imwrite(os.fspath(processed_video_path / filename), img)
-            else:
-                cv2.imwrite(os.fspath(processed_video_path / filename), img * 0)
-            former_im = now_im
+            if now_im is not None:
+                filename = str(n * internal_frame + start_frame) + '_' + str(j) + '.jpg'
+                if np.mean(np.abs(now_im - former_im)) > 5:
+                    img = img * (1 - alpha) + now_im * alpha
+                    cv2.imwrite(os.fspath(processed_video_path / filename), img)
+                else:
+                    cv2.imwrite(os.fspath(processed_video_path / filename), img * 0)
+                former_im = now_im
