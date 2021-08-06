aa3812dc236560f0cbc97c7ec1e81b94061c1908
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-08-06 18:12:54 -0400

    Generalize the reading of directories
    This fixes "IndexError: list index out of range" in vid_utils.py

--- Masks/get_ignore_area.py
+++ Masks/get_ignore_area.py
@@ -1,17 +1,21 @@
+import os
+import pathlib
+import time
+
+import natsort
 import numpy as np
+import tqdm
 from scipy.ndimage.filters import gaussian_filter
 from skimage.measure import label
 
-video_ids = np.arange(1, 101)
-# video_ids = [1]
-count_thred = 0.02
-min_area = 500
-gass_sigma = 2
-score_thred = 0.1
 
-for video_id in video_ids:
+def ignore_area(masks_dir: pathlib.PurePath, mask_txt: pathlib.PurePath, mas_dir: pathlib.Path) -> None:
+    count_thred = 0.02
+    min_area = 500
+    gass_sigma = 2
+    score_thred = 0.1
     dt_results_fbf = {}
-    with open("%s.txt" % video_id, 'r') as f:
+    with open(masks_dir / mask_txt, 'r') as f:
         for line in f:
             line = line.rstrip()
             word = line.split(',')
@@ -26,13 +30,10 @@ for video_id in video_ids:
             if score > score_thred:
                 dt_results_fbf[frame].append([x1, y1, x1 + tmp_w, y1 + tmp_h, score])
 
-    # im = cv2.imread("data/AIC_Track3/ori_images/%s/1.jpg"%video_id)
-
     h = 410
     w = 800
     c = 3
     mat = np.zeros((h, w))
-    # print(len(dt_results_fbf))
     for frame in dt_results_fbf:
         if frame < 18000:
             tmp_score = np.zeros((h, w))
@@ -43,8 +44,7 @@ for video_id in video_ids:
                     score, tmp_score[int(float(box[1])):int(float(box[3])), int(float(box[0])):int(float(box[2]))])
 
             mat = mat + tmp_score
-    # plt.imshow(mat)
-    # plt.show()
+
     mat = mat - np.min(mat)
     mat = mat / np.max(mat)
     mask = mat > count_thred
@@ -58,4 +58,21 @@ for video_id in video_ids:
     mask = mask.astype(float)
     k = gaussian_filter(mask, gass_sigma)
     mask = k > count_thred
-    np.save("Mas/%s.npy" % str(video_id), mask)
+    mask_npy = mas_dir / mask_txt.with_suffix(".npy")
+    mask_npy.parent.mkdir(parents=True, exist_ok=True)
+    np.save(os.fspath(mask_npy), mask)
+
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    masks_dir = pathlib.Path(__file__).resolve().parent
+    masks_txt = iter(map(lambda txt: txt.relative_to(masks_dir), masks_dir.rglob("*.txt")))
+    masks_txt = natsort.natsorted(masks_txt, alg=natsort.ns.PATH)
+    mas_dir = masks_dir / "Mas"
+
+    for mask_txt in tqdm.tqdm(masks_txt):
+        ignore_area(masks_dir, mask_txt, mas_dir)
+
+    end_time = time.perf_counter()
+    res = time.strftime("%Hh:%Mm:%Ss", time.gmtime(end_time - start_time))
+    print(f"Finished in {res}")
--- Seg_masks.py
+++ Seg_masks.py
@@ -3,85 +3,25 @@
 
 # In[1]:
 
-
 import json
+import pathlib
+import time
+from typing import Dict
 
-import numpy as np
+import tqdm
 
-# In[5]:
-
-
-with open('Masks/part2.json', 'r') as f:
-    D = json.load(f)
-
-# In[ ]:
-
-
-Folder = set(np.arange(1, 101)) - set(np.array(
-    [1, 4, 6, 9, 11, 15, 16, 17, 22, 24, 27, 28, 29, 30, 34, 35, 37, 38, 40, 41, 42, 44, 46, 52, 54, 57, 63, 68, 71, 75,
-     76, 79, 80, 81, 82, 84, 89, 96]))
-Folder = list(Folder)
-
-# In[115]:
-
-
-i = 0
-names = ["car", "bus", "truck"]
-
-for frame in D:
-    #     print(frame['filename'],frame['filename'].split('/')[4])
-    if frame['filename'].split('/')[4] == str(Folder[i]):
-
-        file1 = open("Masks/" + str(Folder[i]) + ".txt", "a")
-        #         print(frame['filename'])
-        for bound in frame['objects']:
-            if bound['name'] in names:
-                frame_n = frame['filename'].split('/')[5].split('.')[0]
-                #                 print(frame_n)
-                a = -1
-                b = bound['relative_coordinates']['center_x'] * 800
-                c = bound['relative_coordinates']['center_y'] * 410
-                d = bound['relative_coordinates']['width'] * 800
-                e = bound['relative_coordinates']['height'] * 410
-                f = bound['confidence']
-                g = -1
-                h = -1
-                j = -1
-                line = [frame_n, a, b, c, d, e, f, g, h, j]
-                for l in line:
-                    file1.write(str(l) + ",")
-                file1.write("\n")
-        file1.close()
 
 # In[6]:
 
-
-with open('Masks/part1.json', 'r') as f:
-    D = json.load(f)
-
-# In[7]:
-
-
-np.array(
-    [1, 4, 6, 9, 11, 15, 16, 17, 22, 24, 27, 28, 29, 30, 34, 35, 37, 38, 40, 41, 42, 44, 46, 52, 54, 57, 63, 68, 71, 75,
-     76, 79, 80, 81, 82, 84, 89, 96])
-
-# In[ ]:
-
-
-i = 0
-names = ["car", "bus", "truck"]
-
-for frame in D:
-    #     print(frame['filename'],frame['filename'].split('/')[4])
-    if frame['filename'].split('/')[4] == str(Folder[i]):
-
-        file1 = open("Masks/" + str(Folder[i]) + ".txt", "a")
-        #         print(frame['filename'])
+def seg_masks(frame: Dict) -> None:
+    filename = pathlib.Path(frame['filename'])
+    mask_txt = repo_path / "Masks" / filename.parent.relative_to(repo_path / "ori_images")
+    mask_txt.parent.mkdir(parents=True, exist_ok=True)
+    with open(mask_txt.with_suffix(".txt"), "a") as file1:
         for bound in frame['objects']:
             if bound['name'] in names:
-                frame_n = frame['filename'].split('/')[5].split('.')[0]
-                #                 print(frame_n)
+                # Read the frame number of the filename that is before the underscore.
+                frame_n = filename.stem.split("_")[0]
                 a = -1
                 b = bound['relative_coordinates']['center_x'] * 800
                 c = bound['relative_coordinates']['center_y'] * 410
@@ -95,4 +35,19 @@ for frame in D:
                 for l in line:
                     file1.write(str(l) + ",")
                 file1.write("\n")
-        file1.close()
+
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    repo_path = pathlib.Path(__file__).resolve().parent
+    with open(repo_path / 'Masks' / 'part1.json', 'r') as f:
+        D = json.load(f)
+
+    names = ("car", "bus", "truck")
+
+    for frame in tqdm.tqdm(D):
+        seg_masks(frame)
+
+    end_time = time.perf_counter()
+    res = time.strftime("%Hh:%Mm:%Ss", time.gmtime(end_time - start_time))
+    print(f"Finished in {res}")
