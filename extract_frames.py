a854c086ed51167b519c666e66515549117373dd
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-08-29 17:59:12 -0400

    Implement decord for extracting frames

--- extract_frames.py
+++ extract_frames.py
@@ -3,6 +3,8 @@ import concurrent.futures
 import json
 import os
 import pathlib
+import sys
+import subprocess
 import time
 from typing import TextIO
 
@@ -10,34 +12,60 @@ import cv2
 import natsort
 import numpy as np
 
-
 if __name__ == "__main__":
+    repo_path = pathlib.Path(__file__).resolve().parent
+
+
+    def install_decord(repo_path: pathlib.Path):
+        # Install decord
+        sudo = subprocess.Popen(["sudo", "add-apt-repository", "-y", "ppa:jonathonf/ffmpeg-4"])
+        sudo.wait()
+        sudo = subprocess.Popen(["sudo", "apt-get", "update"])
+        sudo.wait()
+        sudo = subprocess.Popen(
+            ["sudo", "apt-get", "install", "-y", "build-essential", "python3-dev", "python3-setuptools",
+             "make", "cmake", "ffmpeg", "libavcodec-dev", "libavfilter-dev", "libavformat-dev",
+             "libavutil-dev"])
+        os.chdir(repo_path)
+        git = subprocess.Popen(["git", "clone", "--recursive", "https://github.com/dmlc/decord"])
+        git.wait()
+        build_dir = repo_path / "decord/build"
+        build_dir.mkdir(exist_ok=True)
+        os.chdir(build_dir)
+        sudo.wait()
+        cmake = subprocess.Popen(["cmake", "..", "-DUSE_CUDA=ON", "-DCMAKE_BUILD_TYPE=Release"])
+        cmake.wait()
+        cmake = subprocess.Popen("make")
+        cmake.wait()
+        os.chdir("../python")
+        sys.path.append(os.fspath(repo_path / "decord/python"))
+        os.system('python3 setup.py install --user')
+
+
+    install_decord(repo_path)
+    import decord
+
 
     def extract_frames(dest_dir: pathlib.Path, video_path: pathlib.PurePath, root: pathlib.PurePath, time_f: int,
-                       ori_images_txt: TextIO) -> pathlib.Path:
+                       ori_images_txt: TextIO, ctx) -> pathlib.Path:
         """
         :param dest_dir: Directory where the extracted frames will be stored.
         :param video_path: The absolute path of the video.
         :param root: Directory containing the videos to be processed.
         :param time_f: Time frequency.
         :param ori_images_txt: File object where the frames of `video_path` are stored.
+        :param ctx: The context to decode the video file.
         :return: Directory containing the stored frames of `video_path`
         """
-        vc = cv2.VideoCapture(os.fspath(video_path))
-        if vc.isOpened():
-            pic_path = dest_dir / video_path.relative_to(root).with_suffix("")
-            pic_path.mkdir(parents=True, exist_ok=True)
-            c = time_f
-            vc.set(cv2.CAP_PROP_POS_FRAMES, c - 1)
-            while vc.grab():
-                _, frame = vc.retrieve()
-                img_path = os.fspath(pic_path / (str(c) + '.jpg'))
-                cv2.imwrite(img_path, frame)
-                ori_images_txt.write(img_path + "\n")
-                cv2.waitKey(1)
-                c += time_f
-                vc.set(cv2.CAP_PROP_POS_FRAMES, c - 1)
-            vc.release()
+        vr = decord.VideoReader(os.fspath(video_path), ctx=ctx)
+        pic_path = dest_dir / video_path.relative_to(root).with_suffix("")
+        pic_path.mkdir(parents=True, exist_ok=True)
+        frames_indices = list(range(time_f - 1, len(vr), time_f))
+        frames = vr.get_batch(frames_indices).as_numpy()
+        for c, frame in zip(frames_indices, frames):
+            img_path = os.fspath(pic_path / (str(c + 1) + '.jpg'))
+            cv2.imwrite(img_path, frame)
+            ori_images_txt.write(img_path + "\n")
             return pic_path
 
 
@@ -85,16 +113,21 @@ if __name__ == "__main__":
     ext = args.ext.split(" ")
     timeF = args.freq
 
-    repo_path = pathlib.Path(__file__).resolve().parent
     dest_dir = repo_path / "ori_images"
     video_names = frozenset.union(*frozenset(map(lambda e: frozenset(root.rglob("*." + e)), ext)))
     video_names = natsort.natsorted(video_names, alg=natsort.ns.PATH)
     videos_folder = None
     dest_dir_processed = repo_path / "processed_images"
 
+    try:
+        subprocess.check_output("nvidia-smi")
+        ctx = decord.gpu(0)
+    except subprocess.CalledProcessError:
+        ctx = decord.cpu(0)
+
     with open(repo_path / "ori_images.txt", "w") as ori_images_txt, \
             concurrent.futures.ProcessPoolExecutor() as p_exec, concurrent.futures.ThreadPoolExecutor() as t_exec:
-        fs = [t_exec.submit(extract_frames, dest_dir, video_path, root, timeF, ori_images_txt)
+        fs = [t_exec.submit(extract_frames, dest_dir, video_path, root, timeF, ori_images_txt, ctx)
               for video_path in video_names]
 
         for future in concurrent.futures.as_completed(fs):
