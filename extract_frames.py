f119e9c5d0f8a8708ceda23080d51ec2c8733d34
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-08-31 17:29:52 -0400

    Implement decord for extracting frames

--- extract_frames.py
+++ extract_frames.py
@@ -2,40 +2,54 @@ import argparse
 import json
 import os
 import pathlib
+import subprocess
 import time
 from typing import TextIO
 
 import cv2
+import decord
 import natsort
 import numpy as np
 import tqdm
 
 
 def extract_frames(dest_dir: pathlib.Path, video_path: pathlib.PurePath, root: pathlib.PurePath, time_f: int,
-                   ori_images_txt: TextIO) -> pathlib.Path:
+                   ori_images_txt: TextIO, ctx: decord.nd.DECORDContext) -> pathlib.Path:
     """
     :param dest_dir: Directory where the extracted frames will be stored.
     :param video_path: The absolute path of the video.
     :param root: Directory containing the videos to be processed.
     :param time_f: Time frequency.
     :param ori_images_txt: File object where the frames of `video_path` are stored.
+    :param ctx: The context to decode the video file.
     :return: Directory containing the stored frames of `video_path`
     """
-    vc = cv2.VideoCapture(os.fspath(video_path))
-    if vc.isOpened():
-        pic_path = dest_dir / video_path.relative_to(root).with_suffix("")
-        pic_path.mkdir(parents=True, exist_ok=True)
-        c = 1
-        while vc.grab():
-            if c % time_f == 0:
-                img_path = os.fspath(pic_path / (str(c) + '.jpg'))
-                _, frame = vc.retrieve()
-                cv2.imwrite(img_path, frame)
-                ori_images_txt.write(img_path + "\n")
-            c += 1
-            cv2.waitKey(1)
-        vc.release()
-        return pic_path
+    pic_path = dest_dir / video_path.relative_to(root).with_suffix("")
+    pic_path.mkdir(parents=True, exist_ok=True)
+    try:
+        vr = decord.VideoReader(os.fspath(video_path), ctx=ctx)
+        vr.skip_frames(time_f)
+        vr.seek(time_f - 1)
+        size = len(vr)
+        frames_indices = range(time_f - 1, size, time_f)
+        for c in frames_indices:
+            img_path = os.fspath(pic_path / (str(c + 1) + '.jpg'))
+            cv2.imwrite(img_path, vr.next().asnumpy())
+            ori_images_txt.write(img_path + "\n")
+    except decord.DECORDError:
+        vc = cv2.VideoCapture(os.fspath(video_path))
+        if vc.isOpened():
+            c = 1
+            while vc.grab():
+                if c % time_f == 0:
+                    img_path = os.fspath(pic_path / (str(c) + '.jpg'))
+                    _, frame = vc.retrieve()
+                    cv2.imwrite(img_path, frame)
+                    ori_images_txt.write(img_path + "\n")
+                c += 1
+                cv2.waitKey(1)
+            vc.release()
+    return pic_path
 
 
 def process_frames(dest_dir_processed: pathlib.Path, video_path: pathlib.Path, dest_dir: pathlib.PurePath) -> None:
@@ -85,19 +99,20 @@ if __name__ == "__main__":
 
     repo_path = pathlib.Path(__file__).resolve().parent
     dest_dir = repo_path / "ori_images"
+    dest_dir_processed = repo_path / "processed_images"
     video_names = frozenset.union(*frozenset(map(lambda e: frozenset(root.rglob("*." + e)), ext)))
     video_names = natsort.natsorted(video_names, alg=natsort.ns.PATH)
-    video_folders = []
-    print("capture videos")
+
+    try:
+        subprocess.check_output("nvidia-smi")
+        ctx = decord.gpu(0)
+    except subprocess.CalledProcessError:
+        ctx = decord.cpu(0)
+
     with open(repo_path / "ori_images.txt", "w") as ori_images_txt:
         for video_path in tqdm.tqdm(video_names):
-            pic_path = extract_frames(dest_dir, video_path, root, time_f, ori_images_txt)
-            video_folders.append(pic_path)
-
-    dest_dir_processed = repo_path / "processed_images"
-    print("average images")
-    for video_path in tqdm.tqdm(video_folders):
-        process_frames(dest_dir_processed, video_path, dest_dir)
+            pic_path = extract_frames(dest_dir, video_path, root, time_f, ori_images_txt, ctx)
+            process_frames(dest_dir_processed, pic_path, dest_dir)
 
     # Store relative paths to root directory.
     with open(repo_path / "dataset.json", "w") as f:
